e099d04613f76dbace59414a63eee2dc250a596d
akozik <akozik@23bd9d66-29ac-11df-ad47-1d73243ccc9f>
2010-03-10 23:56:33 +0000

    basic data structure and time test

--- trunk/seqexter_2010_03_08.tcl
+++ trunk/seqexter_2010_03_08.tcl
@@ -11,10 +11,16 @@
 ###########################################################
 
 proc SeqExter {argv} {
-
+	
+	global initial_time
+	global sleep_time
+	
 	set input_file    [lindex $argv 0]
 	set file_out_base [lindex $argv 1]
 	set mod_value     [lindex $argv 2]
+	set sleep_time    [lindex $argv 3]
+	
+	Set_Intial_Time
 	
 	Open_Files $input_file $file_out_base
 	
@@ -23,6 +29,29 @@ proc SeqExter {argv} {
 	Close_Files
 	
 	Print_Final_Message
+	
+}
+
+proc Set_Intial_Time { } {
+
+	global initial_time
+	global sleep_time
+	
+	set initial_time [clock format [clock seconds]]
+	
+	puts " Initial Time:  $initial_time "
+	
+	### return $initial_time
+	
+	after $sleep_time
+	
+}
+
+proc Check_Current_Time { } {
+	
+	set current_time [clock format [clock seconds]]
+	
+	return $current_time
 	
 }
 
@@ -44,11 +73,14 @@ proc Open_Files { input_file file_out_base } {
 proc Read_Input_File { mod_value } {
 	
 	global file_in_1
-	set n 1
+	
+	set n 1 
 	
 	while {[gets $file_in_1 current_line] >= 0} {
 	
 		Check_Mod_Status $n $mod_value
+	
+		Create_Basic_Data_Array $n $current_line
 	
 		incr n
 	
@@ -57,10 +89,21 @@ proc Read_Input_File { mod_value } {
 
 proc Check_Mod_Status { n mod_value } {
 	
+	global initial_time
+	
 	set n_mod [expr fmod($n,$mod_value)]
-	if { $n_mod == 0 } {
-	puts $n
+		if { $n_mod == 0 } {
+		set current_time [Check_Current_Time]
+		puts " $n  lines processed  |  Init Time: $initial_time  Current Time: $current_time "
 	}
+	
+}
+
+proc Create_Basic_Data_Array { n current_line } {
+	
+	global basic_data_array
+	
+	set basic_data_array($n) $current_line
 	
 }
 
@@ -82,13 +125,13 @@ proc Print_Final_Message { } {
 	puts "  +----------------------------+  "
 	puts "  |  Well Done - Enjoy Output  |  "
 	puts "  +----------------------------+  "
-	puts "                                 "
+	puts "                                  "
 	
 }
 
-if {$argc != 3} {
+if {$argc != 4} {
 	puts "Program usage:"
-	puts "Input_File,  Output_File,  Mod_Value"
+	puts "Input_File,  Output_File,  Mod_Value, Sleep_Time"
 } else {
 	SeqExter $argv
 }
